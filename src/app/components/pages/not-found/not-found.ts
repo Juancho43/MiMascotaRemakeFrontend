01b3658d530499f5a2d83d763f78fef83d091915
Juan Bravo <bravo43juancho@gmail.com>
2025-07-14 18:32:03 -0300

    add 404 Not Found page with styling and tests

---
+++ src/app/components/pages/not-found/not-found.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {NgOptimizedImage} from '@angular/common';
+
+@Component({
+  selector: 'app-not-found',
+  imports: [
+    NgOptimizedImage
+  ],
+  templateUrl: './not-found.html',
+  styleUrl: './not-found.scss'
+})
+export default class NotFound {
+
+}
