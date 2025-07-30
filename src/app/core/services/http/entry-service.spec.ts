f49ee62aae8470353cbe82fcab17bb572474cf2f
Juan <bravo43juancho@gmail.com>
2025-07-30 19:40:10 -0300

    fix bugs

--- src/app/core/services/http/entry-service.spec.ts
+++ src/app/core/services/http/entry-service.spec.ts
@@ -1,13 +1,19 @@
 import {TestBed} from '@angular/core/testing';
 
 import {EntryService} from './entry-service';
+import {provideHttpClient} from '@angular/common/http';
 
 describe('EntryService', () => {
   let service: EntryService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers:[
+        provideHttpClient()
+      ]
+    });
     service = TestBed.inject(EntryService);
+
   });
 
   it('should be created', () => {
